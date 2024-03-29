c739fc46d9f153185689b2e2869e2e45672da6b3
Ziad Elgendy <165091274+ZiadElgendy21@users.noreply.github.com>
2024-03-29 20:14:16 +0200

    Add files via upload

---
+++ German_Cars_Analysis/Home.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+import streamlit as st
+
+st.markdown("<h1> <center> German Cars Project </center> </h1>",unsafe_allow_html= True)
+st.markdown(''' <center>  <h6>
+    This app is created to analyze data about german cars</center> </h6> ''', unsafe_allow_html=True)
+
+st.image("./Sources/logo.jpg")
